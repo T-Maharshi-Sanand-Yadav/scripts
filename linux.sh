d62c59460b05c8d8f1ce9ccd73fce9f92d7aefab
TMSY Tutorials <104819057+T-Maharshi-Sanand-Yadav@users.noreply.github.com>
2026-03-13 11:50:59 +0530

    Add linux.sh with prompt and color aliases
    Create linux.sh to configure a custom terminal prompt and enable colorized output for common commands. Sets prompt to "[TMSY:%~]: " (previous date-format prompt left commented) and adds aliases: ls, ll, and grep with --color=auto for improved shell usability.

---
+++ linux.sh
@@ -0,0 +1,12 @@
+# Terminal Custom Prompt
+#set prompt="[TMSY:%~] [`date '+%d-%m-%Y %I:%M %p'`]$ "
+set prompt="[TMSY:%~]: "
+
+
+# Enable color support for ls
+alias ls 'ls --color=auto'
+alias ll 'ls -lah --color=auto'
+
+
+# Enable color for grep (if supported)
+alias grep 'grep --color=auto'
